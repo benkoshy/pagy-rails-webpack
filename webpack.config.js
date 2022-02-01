87d3329c8df3b0936c33dff6d3466129e260a1d8
BK <benkoshy@hotmail.com>
2022-02-02 09:55:00 +1100

    fix: export pagy js path so it can be picked up by webpack
    fix: simply node_modules path

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
-const path    = require("path")
+const path = require("path")
 const webpack = require("webpack")
 
-module.exports = {
-  mode: "production",
-  devtool: "source-map",
-  entry: {
-    application: "./app/javascript/application.js"
-  },
-  output: {
-    filename: "[name].js",
-    sourceMapFilename: "[name].js.map",
-    path: path.resolve(__dirname, "app/assets/builds"),
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
+module.exports = (env) => {
+	return {
+		mode: "production",
+		devtool: "source-map",
+		entry: {
+			application: "./app/javascript/application.js"
+		},
+		output: {
+			filename: "[name].js",
+			sourceMapFilename: "[name].js.map",
+			path: path.resolve(__dirname, "app/assets/builds"),
+		},
+		plugins: [
+			new webpack.optimize.LimitChunkCountPlugin({
+				maxChunks: 1
+			})
+		],
+		resolve: {
+			modules: [env.pagyPath, 'node_modules']
+		}
+	}
 }
